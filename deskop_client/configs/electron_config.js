1b8c896a665998aec0dac6d40b53f601e317517b
Christian Leicht <christianlj@ltaps.dk>
2019-11-05 20:08:09 +0100

    fix: updated .gitignore

--- deskop_client/configs/electron_config.js
+++ deskop_client/configs/electron_config.js
@@ -1,16 +1,25 @@
-const { app, BrowserWindow } = require('electron');
+const {app, BrowserWindow} = require('electron');
+const SerialPort = require('serialport')
+const Readline = require('@serialport/parser-readline')
+const port = new SerialPort('COM3', {baudRate: 9600})
+
+const parser = new Readline();
+port.pipe(parser);
+
+parser.on('data', line => console.log(`> ${line}`))
+
 
 const mainWindow = function createWindow() {
-	const window = new BrowserWindow({
-		width: 800,
-		height: 600,
-		webPreferences: {
-			nodeIntegration: true
-		},
-		frame: false
-	});
+    const window = new BrowserWindow({
+        width: 800,
+        height: 600,
+        webPreferences: {
+            nodeIntegration: true
+        },
+        frame: false
+    });
 
-	window.loadURL('http://localhost:1234');
+    window.loadURL('http://localhost:1234');
 };
 
 app.on('ready', mainWindow);
