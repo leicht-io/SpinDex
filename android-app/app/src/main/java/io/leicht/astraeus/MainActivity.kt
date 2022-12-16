580f45cfe7bb9fbcee1b33f11017a31f48aabd64
cle <christian@leicht.io>
2022-12-16 19:38:07 +0100

    feat: added android app skeleton.

--- android-app/app/src/main/java/io/leicht/astraeus/MainActivity.kt
+++ android-app/app/src/main/java/io/leicht/astraeus/MainActivity.kt
@@ -31,9 +31,7 @@ class MainActivity : AppCompatActivity() {
         setupActionBarWithNavController(navController, appBarConfiguration)
 
         binding.fab.setOnClickListener { view ->
-            Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                .setAnchorView(R.id.fab)
-                .setAction("Action", null).show()
+
         }
     }
 
@@ -48,7 +46,7 @@ class MainActivity : AppCompatActivity() {
         // automatically handle clicks on the Home/Up button, so long
         // as you specify a parent activity in AndroidManifest.xml.
         return when (item.itemId) {
-            R.id.action_settings -> true
+            R.id.action_connect -> true
             else -> super.onOptionsItemSelected(item)
         }
     }
